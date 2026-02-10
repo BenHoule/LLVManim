9024776379a098d6f643926b68854db9448085e6
BenHoule <benjhoule@hotmail.com>
2026-02-09 22:24:20 -0500

    edited run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 clang -O0 -fno-strict-aliasing -fno-inline -fno-discard-value-names -S -emit-llvm double.c -o double.ll
 opt -passes='print<postdomtree>' -disable-output double.ll
-opt -passes=dot-callgraph -disable-output double.ll
-dot -Tsvg double.ll.callgraph.dot -o callgraph.svg
